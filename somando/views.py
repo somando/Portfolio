6677a1a45ddb1252a05b05caa54484a0f61bc1b7
Soma Ando <soma.ando.1227@gmail.com>
2024-04-01 14:47:18 +0000

    Edit files

--- somando/views.py
+++ somando/views.py
@@ -5,25 +5,25 @@ from .models import *
 def top(request):
     
     static_profile = StaticProfileData.objects.first()
-    static_profile.description = static_profile.description.replace('\span', "</span> <span>")
+    static_profile.description = static_profile.description.replace('\span', "</span><span>")
     
     profiles = ProfileData.objects.all()
     for profile in profiles:
-        profile.detail = profile.detail.replace('\span', "</span> <span>")
+        profile.detail = profile.detail.replace('\span', "</span><span>")
     
     experiences = ExperienceData.objects.all().order_by('date').filter(show_top=True)
     for experience in experiences:
         date_list = str(experience.date).split('-')
         experience.date = date_list[0] + '/' + date_list[1]
-        experience.title = experience.title.replace('\span', "</span> <span>")
+        experience.title = experience.title.replace('\span', "</span><span>")
     
     products = ProductsData.objects.all().order_by('-date').filter(show_top=True)
     for product in products:
-        product.title = product.title.replace('\span', "</span> <span>")
-        product.event = product.event.replace('\span', "</span> <span>")
-        product.team = product.team.replace('\span', "</span> <span>")
-        product.prize = product.prize.replace('\span', "</span> <span>")
-        product.link_title = product.link_title.replace('\span', "</span> <span>")
+        product.title = product.title.replace('\span', "</span><span>")
+        product.event = product.event.replace('\span', "</span><span>")
+        product.team = product.team.replace('\span', "</span><span>")
+        product.prize = product.prize.replace('\span', "</span><span>")
+        product.link_title = product.link_title.replace('\span', "</span><span>")
     
     skills = SkillData.objects.all().order_by('date')
     
