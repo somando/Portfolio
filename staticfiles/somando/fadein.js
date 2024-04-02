f805902080a3135c7ffa135290bc927264781db5
Soma Ando <soma.ando.1227@gmail.com>
2024-04-02 14:21:37 +0900

    Collect Static Files

---
+++ staticfiles/somando/fadein.js
@@ -0,0 +1,30 @@
+let targets = document.getElementsByClassName('section');
+
+window.addEventListener('scroll', function () {
+    var scroll = window.scrollY;
+    var windowHeight = window.innerHeight;
+    for (let target of targets) {
+        var targetPos = target.getBoundingClientRect().top + scroll;
+        if (!target.classList.contains('is-show')) {
+            if (scroll > targetPos - windowHeight) {
+                target.classList.add('is-show');
+                var animation_wait = target.getElementsByClassName('animation-wait');
+                var i = 0;
+                while (animation_wait.length > i) {
+                    animation_wait[0].classList.remove('animation-wait');
+                }
+            }
+        }
+    }
+});
+
+
+function delay_set(class_name, delay = 0, tolerance = 0.1) {
+
+    var elements = document.getElementsByClassName(class_name);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.animationDelay = String(delay + i * tolerance) + "s";
+    }
+
+}
