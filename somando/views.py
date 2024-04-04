f4afa441e27afe7a0ce402a5d056587e0ea144c2
Soma Ando <soma.ando.1227@gmail.com>
2024-04-04 11:27:48 +0900

    Fixed bugs

--- somando/views.py
+++ somando/views.py
@@ -43,7 +43,7 @@ def experiences(request):
         date_list = str(experience.date).split('-')
         experience.date = date_list[0] + '/' + date_list[1]
         experience.title = experience.title.replace('\span', "</span><span>")
-        experience.detail = experience.detail.replace('\n', "<br>").replace('\span', "</span><span>")
+        experience.detail = experience.detail.replace('\n', "</span><br><span>").replace('\span', "</span><span>")
     
     return render(request, 'somando/experiences.html', {
         'experiences': experiences,
@@ -61,7 +61,7 @@ def products(request):
         product.team = product.team.replace('\span', "</span><span>")
         product.prize = product.prize.replace('\span', "</span><span>")
         product.link_title = product.link_title.replace('\span', "</span><span>")
-        product.detail = product.detail.replace('\n', "<br>").replace('\span', "</span><span>")
+        product.detail = product.detail.replace('\n', "</span><br><span>").replace('\span', "</span><span>")
     
     return render(request, 'somando/products.html', {
         'products': products,
